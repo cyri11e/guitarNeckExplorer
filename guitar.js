39ce46ff67059aa75f5dd1cc6350394fc2827cf4
Cyrille Labbé <cyrille.labbe@equasens.com>
2025-01-29 14:22:06 +0100

    raz de la tonqiue aussi

--- guitar.js
+++ guitar.js
@@ -725,6 +725,7 @@ class Guitar{
             this.clickedNotes = [];
             this.playedNotes = [];
             this.midiPlayedNotes = [];
+            this.tonic = null;
         }
       }
 
